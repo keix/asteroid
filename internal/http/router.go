71d72a1e690eeca2a2d80db4c67d459575e9f1cc
Kei Sawamura <kei@9th.tokyo>
2025-11-18 21:51:28 +0900

    Create authorize prototype

--- internal/http/router.go
+++ internal/http/router.go
@@ -7,10 +7,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterRoutes(r *gin.Engine, keyStore store.KeyStore, cfg config.Config) {
+func RegisterRoutes(
+	r *gin.Engine,
+	keyStore store.KeyStore,
+	userStore store.UserStore,
+	clientStore store.ClientStore,
+	authCodeStore store.AuthCodeStore,
+	cfg config.Config,
+) {
 	r.GET("/.well-known/openid-configuration",
 		oidc.WellKnownHandler(cfg.Issuer))
 
 	r.GET("/jwks.json",
 		oidc.JWKSHandler(keyStore))
+
+	authorizeHandler := oidc.NewAuthorizeHandler(clientStore, userStore, authCodeStore)
+	r.GET("/authorize", authorizeHandler.HandleAuthorize)
 }
